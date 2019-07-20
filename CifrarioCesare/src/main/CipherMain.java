204f870bfeffd0be64ce0bd891de725841799d1c
dennewbie <caruso.denny@gmail.com>
2019-07-20 15:56:16 +0200

    //Comments and bugfix

--- CifrarioCesare/src/main/CipherMain.java
+++ CifrarioCesare/src/main/CipherMain.java
@@ -1,21 +1,21 @@
 package main;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class CipherMain {
-	public static Scanner scanner = new Scanner(System.in);
-	public static String mess = new String();
-	public static List<Character> arr = new ArrayList<Character>();
-	public static List<Character> abc = new ArrayList<Character>();
+	public static Scanner scanner = new Scanner(System.in); //get user input
+	public static String mess = new String(); //user message
+	//List that contains for each position a letter of the user message.
+	public static List<Character> arr = new ArrayList<Character>(); 
+	//List that contains for each position a letter of the Alphabet.
+	public static List<Character> abc = new ArrayList<Character>(); 
 
 	public static void main(String[] args) {		
 		System.out.println("Welcome to the Caesar Cipher.\nInsert a message:");
-		mess = scanner.nextLine();
+		mess = scanner.nextLine(); //taking the user message.
 		
 		int cipherShift = 0;
 		System.out.println("Insert a number (Integer) of cipher that you want to shift:");
-		cipherShift = scanner.nextInt();
+		cipherShift = scanner.nextInt(); //taking the preferred shifting value for the Caesar Cipher.
 		
 		convertToList();
 		createABC();
@@ -29,6 +29,7 @@ public class CipherMain {
 		}
 	}
 	
+	//Fill the arr List with the word's letters (one for each position inside the List.
 	public static void convertToList() {
 		mess = mess.toUpperCase();
 		for (int i=0; i<mess.length(); i++) {
@@ -36,6 +37,7 @@ public class CipherMain {
 		}
 	}
 	
+	//Function that fills the Alphabet List.
 	public static void createABC() {
 		String abcString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		for (int i=0; i<abcString.length(); i++) {
@@ -43,6 +45,10 @@ public class CipherMain {
 		}	
 	}
 	
+	//Function that encrypts the user message by replacing the given index value with another given value.
+	//In our case this given value it's the cipherShift variable that user inserted previously.
+	//If the function will find that we're in the last indexes of the Alphabet List, it will go back to the start.
+	//For instance, if the user choose 3 as cipherShift, 'X' will be 'A', 'Y' will be 'B' and 'Z' will be 'C'. 
 	public static void cryptoLetter(int cipherShift) {
 		for (int i=0; i<arr.size(); i++) {
 			for (int j=0; j<abc.size(); j++) {
