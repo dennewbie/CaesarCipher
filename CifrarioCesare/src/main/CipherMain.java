7456a1966fe2f592cc85797032ad4e547237617c
dennewbie <caruso.denny@gmail.com>
2019-07-21 19:58:22 +0200

    Update Function Encrypting. Now it works also with negative numbers and/or big numbers.

--- CifrarioCesare/src/main/CipherMain.java
+++ CifrarioCesare/src/main/CipherMain.java
@@ -46,31 +46,37 @@ public class CipherMain {
 	}
 	
 	//Function that encrypts the user message by replacing the given index value with another given value.
-	//In our case this given value it's the cipherShift variable that user inserted previously.
+	//In our case this given value it's the cipherShiftUser variable that user inserted previously.
 	//If the function will find that we're in the last indexes of the Alphabet List, it will go back to the start.
 	//For instance, if the user choose 3 as cipherShift, 'X' will be 'A', 'Y' will be 'B' and 'Z' will be 'C'. 
-	public static void cryptoLetter(int cipherShift) {
-		for (int i=0; i<arr.size(); i++) {
-			for (int j=0; j<abc.size(); j++) {
-				if (arr.get(i) == abc.get(j)) {
 
-					if (abc.indexOf(abc.get(j)) > 22) {
-						switch(abc.indexOf(abc.get(j))) {
-						case 23:
-							arr.set(i, abc.get(0 + (cipherShift - 3)));
+	
+	public static void cryptoLetter(int cipherShiftUser) {
+		for (int i=0; i<arr.size(); i++) {
+			if (cipherShiftUser < 0) {
+				if ((abc.indexOf(arr.get(i)) + cipherShiftUser) < 0) {
+					int newCipher = abc.indexOf(arr.get(i)) + cipherShiftUser;
+					while (newCipher < 0) {
+						if ((newCipher + 26) > 0) {
+							arr.set(i, abc.get(newCipher + 26));
 							break;
-						case 24:
-							arr.set(i, abc.get(0 + (cipherShift - 2)));
-							break;
-						case 25:
-							arr.set(i, abc.get(0 + (cipherShift - 1)));
-							break;
-						default:
-							System.out.println("Exception");
-							break;
-						}
-					} else {
-						arr.set(i, abc.get(j + cipherShift));
+						} 
+						newCipher += 26;
+					}
+				}
+			} else if (abc.indexOf(arr.get(i)) + cipherShiftUser > 25) {
+				int newCipher = cipherShiftUser + abc.indexOf(arr.get(i));
+				while (newCipher > 25) {
+					if ((newCipher - 26) < 25) {
+						arr.set(i, abc.get(newCipher - 26));
+						break;
+					}
+					newCipher -= 26;
+				}
+			} else {
+				for (int j=0; j<abc.size(); j++) {
+					if ((abc.get(j)) == arr.get(i)) {
+						arr.set(i, (abc.get(j + cipherShiftUser)));
 						break;
 					}
 				}
